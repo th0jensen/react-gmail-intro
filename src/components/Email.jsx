c0cab4de549f6fa91432cf8a97e7cc318365ce18
Thomas Jensen <thomas.jensen_@outlook.com>
2024-06-19 23:44:29 +0200

    Split into components

---
+++ src/components/Email.jsx
@@ -0,0 +1,11 @@
+import Content from './Email/Content'
+import Toolbar from './Email/Toolbar'
+
+export default function Email() {
+    return (
+        <main className="email-view">
+            <Content />
+            <Toolbar />
+        </main>
+    )
+}
